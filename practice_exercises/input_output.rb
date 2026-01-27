2e5df75bbe481b9938da9275c71b36e452d641aa
Derin DeNio <derindenio@gmail.com>
2026-01-26 23:03:26 -0700

    puts with variables

--- practice_exercises/input_output.rb
+++ practice_exercises/input_output.rb
@@ -3,3 +3,9 @@ puts "I will be printed to the console!"
 p 'I will also be printed to the console!'
 
 print 'Hello world !!'
+
+val1 = "This is variable one"
+val2 = "This is variable two"
+
+puts val1
+puts val2
