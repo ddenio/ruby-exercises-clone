c71c8b317c92312647c3ce14cbdb4355e3358429
Derin DeNio <derindenio@gmail.com>
2026-01-27 22:28:49 -0700

    First commit on loops exercise

--- practice_exercises/input_output.rb
+++ practice_exercises/input_output.rb
@@ -1,11 +1,15 @@
-puts "I will be printed to the console!"
+# puts "I will be printed to the console!"
 
-p 'I will also be printed to the console!'
+# p 'I will also be printed to the console!'
 
-print 'Hello world !!'
+# print 'Hello world !!'
 
-val1 = "This is variable one"
-val2 = "This is variable two"
+# val1 = "This is variable one"
+# val2 = "This is variable two"
 
-puts val1
-puts val2
+# puts val1
+# puts val2
+
+puts "Enter a Value :"
+val = gets.chomp
+puts "Your value is : #{val}!"
