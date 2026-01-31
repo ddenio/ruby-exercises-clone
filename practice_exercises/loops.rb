2a5c640370cf670b299d40da95c30e58de9e4ad8
Derin DeNio <derindenio@gmail.com>
2026-01-31 01:29:05 -0700

    Arrays with for loop

--- practice_exercises/loops.rb
+++ practice_exercises/loops.rb
@@ -53,12 +53,20 @@
 
 
 #for loops
-  puts "Enter a number: "
-  x = gets.chomp.to_i
+  # puts "Enter a number: "
+  # x = gets.chomp.to_i
 
-  for i in 1..x do
-    puts x - i
-  end
+  # for i in 1..x do
+  #   puts x - i
+  # end
 
-  puts "Done!"
+  # puts "Done!"
+
+  x = [1, 2, 3, 4, 5]
+
+for i in x.reverse do
+  puts i
+end
+
+puts "Done!"
 
