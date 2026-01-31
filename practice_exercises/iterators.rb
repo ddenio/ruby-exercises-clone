bbf7bf766d33f3213d2ea7cfe3814ad8f689f52d
Derin DeNio <derindenio@gmail.com>
2026-01-31 01:56:13 -0700

    Launch School Loops/iterators Exercise 2

--- practice_exercises/iterators.rb
+++ practice_exercises/iterators.rb
@@ -4,10 +4,22 @@
 
 # names.each { |name| puts name }
 
-names = ['Bob', 'Joe', 'Steve', 'Janice', 'Susan', 'Helen']
-x = 1
+# names = ['Bob', 'Joe', 'Steve', 'Janice', 'Susan', 'Helen']
+# x = 1
 
-names.each do |name|
-  puts "#{x}. #{name}"
-  x += 1
+# names.each do |name|
+#   puts "#{x}. #{name}"
+#   x += 1
+# end
+
+# Exercise 2
+# Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". 
+# Each loop can get info from the user.
+
+output = gets.chomp
+while output != "STOP"
+  puts "Try again!"
+  output = gets.chomp
 end
+
+puts "Done!"
