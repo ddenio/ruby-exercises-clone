61be96af79e64325ce7ea6b77b32b38884c2b711
Derin DeNio <derindenio@gmail.com>
2026-02-03 08:24:33 -0700

    LaunchSchool arrays exercise 8

--- practice_exercises/arrays.rb
+++ practice_exercises/arrays.rb
@@ -48,6 +48,19 @@
 # Exercise 7
 # Use the each_with_index method to iterate through an array of your creation that prints each index and value of the array.
 
-arr = ["John", "Tim", "Derin", "Karen", "Tom", "Matthew"]
+  # arr = ["John", "Tim", "Derin", "Karen", "Tom", "Matthew"]
 
-arr.each_with_index { |val, ind| puts "#{ind+1}. #{val}"}
+  # arr.each_with_index { |val, ind| puts "#{ind+1}. #{val}"}
+
+# Exercise 8
+# Write a program that iterates over an array and builds a new array that is the result of incrementing each value in the original array by a value of 2.
+#  You should have two arrays at the end of this program, The original array and the new array you've created. 
+#  Print both arrays to the screen using the p method instead of puts.
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+arr2 = arr1.map { |num| num+=2 }
+
+p arr1
+p arr2
+
