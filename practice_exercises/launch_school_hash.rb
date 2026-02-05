71d6ec331c82555014acdea6d7a555e9852cde09
Derin DeNio <derindenio@gmail.com>
2026-02-05 09:20:01 -0700

    Launch School Hashes Exercise 1

--- practice_exercises/launch_school_hash.rb
+++ practice_exercises/launch_school_hash.rb
@@ -14,14 +14,22 @@
 #   puts "Bob's #{key} is #{value}"
 # end
 
-def greeting(name, options = {})
-  if options.empty?
-    puts "Hi, my name is #{name}"
-  else
-    puts "Hi, my name is #{name} and I'm #{options[:age]}" +
-         " years old and I live in #{options[:city]}."
-  end
-end
+# def greeting(name, options = {})
+#   if options.empty?
+#     puts "Hi, my name is #{name}"
+#   else
+#     puts "Hi, my name is #{name} and I'm #{options[:age]}" +
+#          " years old and I live in #{options[:city]}."
+#   end
+# end
 
-greeting("Bob")
-greeting("Bob", {age: 62, city: "New York City"})
+# greeting("Bob")
+# greeting("Bob", {age: 62, city: "New York City"})
+
+name_and_age = { "Bob" => 42, "Steve" => 31, "Joe" => 19}
+
+puts name_and_age.fetch("Steve")
+puts name_and_age.fetch("Larry", "Larry isn't in this hash")
+
+puts name_and_age.select { |k,v| (k=="Steve") || (v==42)}
+print name_and_age.to_a
