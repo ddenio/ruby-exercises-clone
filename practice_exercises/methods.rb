b0fb74a859c49f44df26fcb111b0ca24c84b024b
Derin DeNio <derindenio@gmail.com>
2026-02-06 16:52:18 -0700

    Launch School Methods Exercises 4 and 5

--- practice_exercises/methods.rb
+++ practice_exercises/methods.rb
@@ -12,8 +12,29 @@
 # Exercise 3
 # Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
 
-def multiply(a, b)
-  a * b
+# def multiply(a, b)
+#   a * b
+# end
+
+# puts multiply(5,4)
+
+# Exercise 4
+# What will the following code print to the screen?
+  # my guess: will print nothing!
+# def scream(words)
+#   words = words + "!!!!"
+#   return
+#   puts words
+# end
+
+# puts scream("Yippeee")
+
+# Exercise 5
+# 1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+
+
+def scream(words)
+  words = words + "!!!!"
 end
 
-puts multiply(5,4)
+puts scream("Yippeee")
